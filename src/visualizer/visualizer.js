260e76c5ca2f920f708efa83a5bd8e4d38379fae
shahadathb <hridomheart@gmail.com>
2020-08-06 21:19:15 +0600

    replace lines with bar

--- src/visualizer/visualizer.js
+++ src/visualizer/visualizer.js
@@ -19,43 +19,35 @@ export default function visualize(stream) {
   const bufferLength = analyser.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
 
-  requestAnimationFrame(draw);
   function draw() {
-    canvasCtx.clearRect(0, 0, canvas.clientHeight, canvas.clientWidth);
+    canvasCtx.clearRect(0, 0, canvas.height, canvas.width);
     analyser.getByteFrequencyData(dataArray);
     requestAnimationFrame(draw);
 
-    var step = (2 * Math.PI) / bufferLength; // see note 1
-    var h = canvas.width / 2;
-    var k = canvas.height / 2;
-    var r = 40;
+    const x = canvas.width / 2;
+    const y = canvas.height / 2;
 
-    canvasCtx.fillStyle = "rgb(0, 0, 0)";
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-    canvasCtx.lineWidth = 1;
-    canvasCtx.strokeStyle = "red";
+    const radius = 200;
+    const radiusStep = (Math.PI * 2) / bufferLength; // divide circumfurance of circle with buffer length to produce radian for individual bar
 
-    canvasCtx.beginPath(); //tell canvas to start a set of lines
+    for (let i = 0; i < bufferLength; i += 3) {
+      const radian = radiusStep * i; // radian for every byte frequncy data
 
-    for (let i = 0; i < bufferLength; i += 2) {
-      const angle = step * i;
-      const distance = dataArray[i];
+      // co-ordinates of radian in canvas
+      const outerX = x + radius * Math.sin(radian);
+      const outerY = y - radius * Math.cos(radian);
 
-      var x = h + r * Math.cos(angle);
-      var y = k - r * Math.sin(angle); //note 2.
+      const freq = dataArray[i];
 
-      canvasCtx.lineTo(x, y);
-      canvasCtx.moveTo(x, y);
-
-      var visX = x + (distance * Math.cos(angle)) / 2;
-      var visY = y - (distance * Math.sin(angle)) / 2; //note 2.
-
-      canvasCtx.lineTo(visX, visY);
+      canvasCtx.save();
+      canvasCtx.translate(outerX, outerY);
+      canvasCtx.rotate(radian);
+      canvasCtx.fillRect(0, y, 10, 5 + freq);
+      canvasCtx.restore();
     }
-
-    canvasCtx.closePath(); //close the end to the start point
-    canvasCtx.stroke();
   }
+
+  draw();
 }
 
 // draw an oscilloscope of the current audio source
